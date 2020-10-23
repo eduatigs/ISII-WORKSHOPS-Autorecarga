dfd9f51e95b3cd4a5047b843165427dcabe8931f
eduatigs <eduatigs96@gmail.com>
2020-10-22 20:05:39 -0500

    Metodo para el calculo del Premium

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -4,20 +4,31 @@ import ec.edu.espol.workshops.second.models.Customer;
 
 public class CarInsurance {
 
-	
-	
-	
-	private boolean isValidCarInsurance(Customer customer, float calculatedPremium) {
-		
-		return false;
-		
-	}
-	
-private float calculateCarInsurance(Customer customer) {
-		
-		return 0f;
+	private boolean isValidCarInsurance(Customer customer) {
+		if(customer.getAge() >= Constants.MAX_AGE && Constants.IS_REQUIRED_LICENSE) {
+			return false;
+		}
+		return true;
 		
 	}
 	
+	private float calculateCarInsurance(Customer customer) {
+		float total = Constants.BASE_PREMIUM;
+		if(customer.getAge() < 25 && !customer.isMaritalStatus() && customer.getSex() == "M"){
+			total = total + Constants.ADD_PREMIUM_FOR_YOUNGER_NOT_MARRIED;
+		}else if(customer.getSex() == "F" || customer.isMaritalStatus()) {
+			total = total - Constants.SUBSTRACT_PREMIUM_FOR_FEMALE_MARRIED;
+		}else if(customer.getAge() >= 45 && customer.getAge() < 65) {
+			total = total - Constants.SUBSTRACT_PREMIUM_FOR_RANGE_AGE;
+		}
+		return total;
+	}
 	
+	public float getPremiumInsurance(Customer customer) {
+		if(isValidCarInsurance(customer)) {
+			return calculateCarInsurance(customer);
+		}else {
+			return -1;
+		}
+	}
 }
--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/constants/Constants.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/constants/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
 	public static final boolean IS_REQUIRED_LICENSE = true;
 	public static final float SUBSTRACT_PREMIUM_FOR_FEMALE_MARRIED = 200f;
 	public static final float SUBSTRACT_PREMIUM_FOR_RANGE_AGE = 100f;
-	public static final float ADD_PREMIUM_FOR_YOUNGER_NOT_MARRIED= 200f;
+	public static final float ADD_PREMIUM_FOR_YOUNGER_NOT_MARRIED = 1500f;
 
 	private Constants() {
 	    throw new IllegalStateException("Utility class");

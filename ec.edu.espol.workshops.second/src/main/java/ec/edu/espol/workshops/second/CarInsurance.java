b15cbb17d49d98421761a76cc598d52684e18ac3
eduatigs <eduatigs96@gmail.com>
2020-10-29 22:52:08 -0500

    SpotBugs: zero bugs

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -6,18 +6,16 @@ import ec.edu.espol.workshops.second.models.Customer;
 public class CarInsurance {
 
 	private boolean isValidCarInsurance(Customer customer) {
-		
 		return customer.getAge() >= Constants.MAX_AGE_FOR_INSURANCE && Constants.IS_REQUIRED_LICENSE;
-		
 	}
-	
+
 	private float calculateCarInsurance(Customer customer) {
 		float total = Constants.BASE_PREMIUM;
-		if(customer.getAge() < Constants.YOUNG_AGE && !customer.isMaritalStatus() && customer.getSex().equals(Constants.MALE_SEX)){
+		if (customer.getAge() < Constants.YOUNG_AGE && !customer.isMaritalStatus() && customer.getSex().equals(Constants.MALE_SEX)){
 			total = total + Constants.ADD_PREMIUM_FOR_YOUNGER_NOT_MARRIED;
-		}else if(customer.getSex().equals(Constants.FEMALE_SEX) || customer.isMaritalStatus()) {
+		} else if (customer.getSex().equals(Constants.FEMALE_SEX) || customer.isMaritalStatus()) {
 			total = total - Constants.SUBSTRACT_PREMIUM_FOR_FEMALE_MARRIED;
-		}else if(customer.getAge() >= Constants.MID_MIN_AGE_FOR_RANGE && customer.getAge() < Constants.MID_MAX_AGE_FOR_RANGE) {
+		} else if (customer.getAge() >= Constants.MID_MIN_AGE_FOR_RANGE && customer.getAge() < Constants.MID_MAX_AGE_FOR_RANGE) {
 			total = total - Constants.SUBSTRACT_PREMIUM_FOR_RANGE_AGE;
 		}
 		return total;

a225f00f25a925cf24e0503f448e7486c9e00aed
eduatigs <eduatigs96@gmail.com>
2020-11-27 21:56:10 -0500

    Cambios al Main

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/Main.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/Main.java
@@ -16,7 +16,7 @@ public class Main {
 	    
 		System.out.println ("Ingrese el Marital Status:");
 		String maritalStatus = entradaEscaner.nextLine (); 
-		boolean maritalStatusBool=maritalStatus=="true";
+		boolean maritalStatusBool=maritalStatus.equals("true");
 		int age = -1;
 		try {
 			age = Integer.parseInt(edad);

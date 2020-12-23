5459c6191d06835bed5d326f03e0c4aeacc0e738
Iliana <iliesbol@espol.edu.ec>
2020-12-23 07:23:31 -0500

    Testing

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -4,13 +4,16 @@ import ec.edu.espol.workshops.second.constants.Constants;
 import ec.edu.espol.workshops.second.models.Customer;
 /*CLase que maneja la prima*/
 public class CarInsurance {
-
-	private static boolean isValidCarInsurance(Customer customer) {
-		
-		return customer.getAge() <= Constants.MAX_AGE_FOR_INSURANCE && Constants.IS_REQUIRED_LICENSE;
-		
-	}
 	
+	/*Obiene el valor de la prima*/
+	public  static float getPremiumInsurance(Customer customer) {
+		System.out.println(customer);
+		if(isValidCarInsurance(customer)) {
+			return calculateCarInsurance(customer);
+		}else {
+			return -1.0f;
+		}
+	}
 	private static float calculateCarInsurance(Customer customer) {
 		float total = Constants.BASE_PREMIUM;
 		if(customer.getAge() < Constants.YOUNG_AGE && !customer.isMaritalStatus() && customer.getSex().equals(Constants.MALE_SEX)){
@@ -22,13 +25,8 @@ public class CarInsurance {
 		}
 		return total;
 	}
-	/*Obiene el valor de la prima*/
-	public  static float getPremiumInsurance(Customer customer) {
-		System.out.println(customer);
-		if(isValidCarInsurance(customer)) {
-			return calculateCarInsurance(customer);
-		}else {
-			return -1.0f;
-		}
+	private static boolean isValidCarInsurance(Customer customer) {
+		return customer.getAge() <= Constants.MAX_AGE_FOR_INSURANCE && Constants.IS_REQUIRED_LICENSE;	
 	}
+
 }
